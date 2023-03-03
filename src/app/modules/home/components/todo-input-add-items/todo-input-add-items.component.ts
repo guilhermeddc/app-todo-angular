800f5709c407fddf0066f05ac0a0ad782f037e18
Guilherme Rodrigues <guilhermeddc@gmail.com>
2023-03-03 01:12:22 -0300

    feat: completed

--- src/app/modules/home/components/todo-input-add-items/todo-input-add-items.component.ts
+++ src/app/modules/home/components/todo-input-add-items/todo-input-add-items.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-todo-input-add-items',
   templateUrl: './todo-input-add-items.component.html',
-  styleUrls: ['./todo-input-add-items.component.scss']
+  styleUrls: ['./todo-input-add-items.component.scss'],
 })
 export class TodoInputAddItemsComponent {
+  @Output() public emitItemTaskList = new EventEmitter<string>();
 
+  public addItemTaskList: string = '';
+
+  public submitItemTaskList(): void {
+    this.addItemTaskList = this.addItemTaskList.trim();
+
+    if (this.addItemTaskList) {
+      this.emitItemTaskList.emit(this.addItemTaskList);
+      this.addItemTaskList = '';
+    }
+  }
 }
--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
+
+// Model
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
-export class TodoListComponent {
+export class TodoListComponent implements DoCheck {
+  public taskList: TaskList[] = JSON.parse(
+    localStorage.getItem('taskList') || '[]'
+  );
 
+  public ngDoCheck(): void {
+    this.setLocalStorage();
+  }
+
+  public setEmitTaskList(task: string): void {
+    this.taskList.push({
+      task: task,
+      checked: false,
+    });
+  }
+
+  public deleteItemTaskList(index: number): void {
+    this.taskList.splice(index, 1);
+  }
+
+  public deleteAllTaskList(): void {
+    const confirm = window.confirm('Você deseja realmente deletar tudo?');
+
+    if (confirm) {
+      this.taskList = [];
+    }
+  }
+
+  public validationInput(event: string, index: number): void {
+    if (!event.length) {
+      const confirm = window.confirm('Tarefa esta vazia, deseja deletar?');
+
+      if (confirm) this.deleteItemTaskList(index);
+    }
+  }
+
+  public setLocalStorage(): void {
+    if (this.taskList) {
+      this.taskList.sort(
+        (first, last) => Number(first.checked) - Number(last.checked)
+      );
+
+      localStorage.setItem('taskList', JSON.stringify(this.taskList));
+    }
+  }
 }
